38603acd653277b0f38755c5d6ac7d62bf9ea5c8
Richard <einetunet@gmail.com>
2021-04-21 22:01:41 +0200

    Test

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import com.example.domain.Person;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get(produces = MediaType.APPLICATION_JSON)
+    public Person index() {
+        return new Person("Ole");
+    }
+}
